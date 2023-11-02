d2a4ca90ecc62fd177b86d09403bb46e335248a8
KYW <qodna25@gmail.com>
2023-11-02 18:05:29 +0900

    2. 모션파스 Getter 와 Setter 메서드

--- advanced/advanced-start/01.Animation Control/1-1.motionPath/main.js
+++ advanced/advanced-start/01.Animation Control/1-1.motionPath/main.js
@@ -1,9 +1,24 @@
-const $ = (node) => document.querySelector(node);
+const domSelector = (node) => document.querySelector(node);
+
+let animation = gsap.to("#tiger", {
+  duration: 6,
+  ease: "none",
+  motionPath: {
+    path: "#route",
+    align: "#tiger",
+  },
+});
+
+let progress = "";
+function render() {
+  progress = domSelector(".progress").style.cssText = `width:${
+    animation.progress() * 100
+  }%`;
 
 
+if(animation.progress()>=1) return  
+  requestAnimationFrame(render);
+}
 
-
-
-
-
-
+render();
+console.log(progress);
